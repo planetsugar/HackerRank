e116400378fdcf856332203e05d1ebd5315ed0a7
planetsugar <paulkeller@planetsugar.co.uk>
2020-05-30 20:05:34 +0100

    Completed SprialMatrix using for loops

--- SpiralMatrix/matrix.js
+++ SpiralMatrix/matrix.js
@@ -1,13 +1,47 @@
 const matrix = size => {
-    let counter = 1;
-    let colStart = 1;
-    let colEnd = size;
-    let rowStart = 1;
-    let rowEnd = size;
-
-    const matrix=[];
-    for(let col=colStart-1;col<colEnd-1;col++) {
         
+    const range = range => [...Array(range).values()];
+    const insert = (r, c, v) => returnMatrix[r][c] = v;
+    const empty = (r, c) => returnMatrix[r][c] === undefined && r;
+
+    const returnMatrix = [];
+
+    for (let row = 0; row < size; row++) {
+        returnMatrix.push(range(size));
     }
 
+    let left = 0;
+    let right = size - 1;
+    let top = 0;
+    let bottom = size - 1;
+
+    //for (let count = 1; count < size + 1; count++) {
+        // Move right for loop 
+    let counter = 1;
+
+    while (counter<=(size * size)) {
+        for(let column = left; column<=right; column++) {
+            insert(top, column, counter++);
+        }
+        top++;
+
+        for (let row=top;row<=bottom;row++) {
+            insert(row, right, counter++);
+        }
+        right--;
+
+        for (let column=right;column>=left;column--) {
+            insert(bottom, column, counter++);
+        }
+        bottom--;
+
+        for (let row=bottom;row>=top;row--) {
+            insert(row, left, counter++);
+        }
+        left++;
+    }
+
+    return returnMatrix;
 }
+
+console.log(matrix(5));
